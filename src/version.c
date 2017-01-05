18842546d79d222824e728c6b2466a5675de2afd
Dan Dees <dand@appsmiths.com>
2017-01-05 04:34:57 +0000

    handle configure not being run

--- src/version.c
+++ src/version.c
@@ -1,5 +1,9 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
+#else
+// using CMake, configure not run
+#define PACKAGE_STRING "Bison++ Version 1.21-8"
 #endif
 
 char *version_string = PACKAGE_STRING ", adapted from GNU bison by coetmeur@icdc.fr";
+
