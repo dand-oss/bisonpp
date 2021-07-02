943afb7cc6538d229ad3fe8eb81d39ddcda1d410
Dan Dees <dand@appsmiths.com>
2021-07-02 16:58:07 -0500

    version.c - restore package name

--- src/version.c
+++ src/version.c
@@ -2,4 +2,4 @@
 #include "config.h"
 #endif
 
-char *version_string = PACKAGE_STRING ", adapted from GNU bison by coetmeur@icdc.fr";
+char *version_string = "bison++ Version 1.21-8, adapted from GNU bison by coetmeur@icdc.fr";
