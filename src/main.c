805808906fb6cae6074e4ec07be8aecd72046688
Dan Dees <dand@appsmiths.com>
2024-07-29 12:02:12 +0700

    new gcc requires vrargs to wrap sprintf

--- src/main.c
+++ src/main.c
@@ -108,13 +108,16 @@ char *s;
    functions.  This is the ONLY safe way to write such a function.  */
 /*VARARGS1*/
 
+#include <stdarg.h>
+
 void
-fatals(fmt,x1,x2,x3,x4,x5,x6,x7,x8)
-char *fmt;
+fatals(char* fmt,...)
 {
   char buffer[200];
-
-  sprintf(buffer, fmt, x1,x2,x3,x4,x5,x6,x7,x8);
+  va_list va;
+  va_start(va, fmt);
+  vsprintf(buffer, fmt, va);
+  va_end(va);
   fatal(buffer);
 }
 
